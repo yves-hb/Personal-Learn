475a30232af47d72e717bcb8ac22b10d39b0a118
yves_hb <hb_yves@163.com>
2023-05-30 17:53:52 +0800

    ‘响应式基础

--- Front-End/4-Vue/test/day01/demo01/src/App.vue
+++ Front-End/4-Vue/test/day01/demo01/src/App.vue
@@ -1,53 +1,28 @@
-<!-- <script setup>
-/**
- * import 引入其他组件配置
- * 语法: import 组件名 form 组件相对路径
- */
-import HelloWorld from './components/HelloWorld.vue'
-</script> -->
-
 <script>
-export default{
-  data(){
-    return{
-      name:"张三",
-      age:20
+export default {
+  data() {
+    return {
+      isActive: true,
+      error: {}
+    }
+  },
+  computed: {
+    classObject() {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error
+      }
     }
   }
 }
-
 </script>
-
 <template>
-  <div class="logo">
-    <p>你好: {{name}}</p>
-    <span>{{age}}</span>
+  <div class="static" :class="classObject">
+    这是内容
   </div>
 </template>
-
-<!-- <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="HelloWorld" />
-</template> -->
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.active{
+  font-size: 50px;
 }
 </style>
